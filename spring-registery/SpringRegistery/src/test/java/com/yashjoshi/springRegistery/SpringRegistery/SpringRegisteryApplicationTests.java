0f39781476b2eab435b4638f3ff4ae1281fd9b3b
Yash Joshi <yashjoshi1485@gmail.com>
2024-12-17 19:45:30 +0530

    Add files via upload

---
+++ spring-registery/SpringRegistery/src/test/java/com/yashjoshi/springRegistery/SpringRegistery/SpringRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashjoshi.springRegistery.SpringRegistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
